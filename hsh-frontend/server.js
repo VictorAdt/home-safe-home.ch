dcdcfbac4d04c6bad1d0f09cdf9341449ef46e0f
VictorAdt <audetat.v@gmail.com>
2021-01-14 18:15:39 +0100

    server.js

--- hsh-frontend/server.js
+++ hsh-frontend/server.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path');
 
 app.get('/rebuild', (req, res) => {
   res.send('rebuild!')
 })
 
-app.get('/website', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/public'))
 })
 
