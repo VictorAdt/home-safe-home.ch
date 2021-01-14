5664cc2ad87fb79c6bb4cbfe38599cc3b6f66f41
VictorAdt <audetat.v@gmail.com>
2021-01-14 18:09:45 +0100

    server.js

--- hsh-frontend/server.js
+++ hsh-frontend/server.js
@@ -3,10 +3,15 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/rebuild', (req, res) => {
+  res.send('rebuild!')
+})
+
+app.get('/website', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public'))
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
+
