1abc34cef5767ecc901b78a85e3a4e447f288b49
VictorAdt <audetat.v@gmail.com>
2021-01-14 18:28:23 +0100

    server.js

--- hsh-frontend/server.js
+++ hsh-frontend/server.js
@@ -3,9 +3,15 @@ const express = require('express')
 const app = express()
 const port = 8080
 const path = require('path');
+var exec = require('child_process').exec;
 
 app.get('/rebuild', (req, res) => {
-  res.send('rebuild!')
+  exec("gatsby build", function(error, stdout, stderr) {
+    if (!error) {
+      console.log('rebuild');
+    } else {
+      console.log('failed');
+    }
 })
 
 app.use(express.static('public'));
