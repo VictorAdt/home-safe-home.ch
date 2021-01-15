a602890559ad917f67e19fb2c9548a966564744d
VictorAdt <audetat.v@gmail.com>
2021-01-15 15:53:29 +0100

    fck

--- hsh-backend/config/server.js
+++ hsh-backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8082),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a7fd829d72533085426a8202713b9882'),
