07f73aeed4d39e8fa73b771488e5139b733647ee
VictorAdt <audetat.v@gmail.com>
2021-01-18 16:20:39 +0100

    changing hook url with lan ip

--- hsh-backend/api/atelier/models/atelier.js
+++ hsh-backend/api/atelier/models/atelier.js
@@ -12,21 +12,21 @@ module.exports = {
   lifecycles: {
     async afterCreate(entry) {
     console.log('oupss')
-      axios.post('http://frontend.homesafehome.ch', entry)
+      axios.post('http://10.102.3.245/rebuild', entry)
         .catch(() => {
           // Ignore
         }
         );
     },
     async afterUpdate(entry) {
-      axios.post('http://frontend.homesafehome.ch', entry)
+      axios.post('http://frontend.homesafehome.ch/rebuild', entry)
         .catch(() => {
           // Ignore
         }
         );
     },
     async afterDestroy(entry) {
-      axios.post('http://frontend.homesafehome.ch', entry)
+      axios.post('http://frontend.homesafehome.ch/rebuild', entry)
         .catch(() => {
           // Ignore
         }
