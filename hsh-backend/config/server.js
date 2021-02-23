7875a9810ba7bd09f73a870c84a1414a66eab6c5
samuelgoumaz <62564497+samuelgoumaz@users.noreply.github.com>
2021-02-23 18:12:26 +0100

    Update configuration

--- hsh-backend/config/server.js
+++ hsh-backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('PROD', '10.102.5.192'),
+  port: env.int('NODE_PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '92be7c266b9c850bd738aebbddcf940f'),
